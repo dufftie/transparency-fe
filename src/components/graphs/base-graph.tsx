c8f91739966a4b0cbbd3465c81c7edae209ac310
dufftie <dufftie@gmail.com>
2025-03-07 14:51:48 +0200

    Added animation for graphs

--- src/components/graphs/base-graph.tsx
+++ src/components/graphs/base-graph.tsx
@@ -6,6 +6,7 @@ import classNames from 'classnames';
 import { ResponsiveContainer } from 'recharts';
 import { Spin } from 'antd';
 import { fetchData } from '@/src/lib/services/api';
+import { LoadingOutlined } from '@ant-design/icons';
 
 interface BaseGraphProps {
   graphName?: string;
@@ -17,6 +18,23 @@ interface BaseGraphProps {
 const BaseGraph = ({ fetchUrl, processData, children, graphName }: BaseGraphProps) => {
   const [data, setData] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
+  const [showSpinner, setShowSpinner] = useState(true);
+
+  useEffect(() => {
+    let timer: NodeJS.Timeout;
+
+    if (loading) {
+      setShowSpinner(true);
+    } else {
+      timer = setTimeout(() => {
+        setShowSpinner(false);
+      }, 300);
+    }
+
+    return () => {
+      if (timer) clearTimeout(timer);
+    };
+  }, [loading]);
 
   const loadData = useCallback(
     debounce(async () => {
@@ -24,7 +42,6 @@ const BaseGraph = ({ fetchUrl, processData, children, graphName }: BaseGraphProp
 
       setLoading(true);
       try {
-        // Normalize the endpoint
         const endpoint = fetchUrl.startsWith('http')
           ? fetchUrl
           : fetchUrl.startsWith('/') ? fetchUrl : `/${ fetchUrl }`;
@@ -34,7 +51,9 @@ const BaseGraph = ({ fetchUrl, processData, children, graphName }: BaseGraphProp
       } catch (error) {
         console.error('Error fetching data:', error);
       } finally {
-        setLoading(false);
+        setTimeout(() => {
+          setLoading(false);
+        }, 300);
       }
     }, 1000),
     [fetchUrl, processData],
@@ -45,12 +64,10 @@ const BaseGraph = ({ fetchUrl, processData, children, graphName }: BaseGraphProp
   }, [loadData]);
 
   return (
-    <div className={ classNames('graph', graphName) }>
-      { loading && (
-        <div className="graph-loading-spinner">
-          <Spin size="large" />
-        </div>
-      ) }
+    <div className={ classNames('graph', graphName, { 'loading': loading }) }>
+      <div className={ classNames('graph-loading-spinner', { 'active': showSpinner }) }>
+        <Spin size="large" indicator={ <LoadingOutlined spin /> } className={ classNames({ 'active': showSpinner }) } />
+      </div>
       <ResponsiveContainer width="100%" height="100%">
         { children(data, loading) }
       </ResponsiveContainer>
