03f479a15b90129e219976644ac3b50c9b2e5b99
dufftie <dufftie@gmail.com>
2025-04-20 17:12:46 +0300

    Limited entries count in proceeded-article-graph to 500 per time

--- src/components/graphs/proceeded-article-graph.tsx
+++ src/components/graphs/proceeded-article-graph.tsx
@@ -37,16 +37,111 @@ const ProceededArticlesGraph = ({ mediaId }: ProceededArticlesGraphProps) => {
     }));
   }, []); // No dependencies as it doesn't use any external values
 
-  // Filter data based on domain range - this will run during render but won't trigger refetch
-  const filterDataByDomain = (data: any[]) => {
+  // Filter and aggregate data based on the number of data points
+  const filterAndAggregateData = (data: any[]) => {
     const [domainStart, domainEnd] = formattedDomainDateRange;
     const domainStartDate = dayjs(domainStart);
     const domainEndDate = dayjs(domainEnd);
 
-    return data.filter(entry => {
+    // Filter data to domain range
+    const filteredData = data.filter(entry => {
       const entryDate = dayjs(entry.date);
       return entryDate.isSameOrAfter(domainStartDate) && entryDate.isSameOrBefore(domainEndDate);
     });
+
+    // If we have 500 or fewer data points, return all of them
+    if (filteredData.length <= 500) {
+      return filteredData;
+    }
+
+    // Determine aggregation period based on the number of data points
+    let aggregationPeriod: 'day' | 'week';
+    
+    // Calculate how many data points we'd have with each aggregation level
+    const uniqueDays = new Set(filteredData.map(entry => dayjs(entry.date).format('YYYY-MM-DD'))).size;
+    const uniqueWeeks = new Set(filteredData.map(entry => dayjs(entry.date).startOf('week').format('YYYY-MM-DD'))).size;
+    
+    // Choose the smallest aggregation period that gives us 500 or fewer data points
+    if (uniqueWeeks <= 500) {
+      aggregationPeriod = 'week';
+    } else {
+      aggregationPeriod = 'day';
+    }
+
+    // Group data by aggregation period
+    const aggregatedData: Record<string, any> = {};
+
+    filteredData.forEach(entry => {
+      let key: string;
+      
+      if (aggregationPeriod === 'day') {
+        key = dayjs(entry.date).format('YYYY-MM-DD');
+      } else {
+        // Week
+        key = dayjs(entry.date).startOf('week').format('YYYY-MM-DD');
+      }
+
+      if (!aggregatedData[key]) {
+        aggregatedData[key] = {
+          date: entry.date,
+          timestamp: dayjs(key).valueOf(),
+          displayDate: dayjs(key).format('YYYY'),
+          analysed_count: 0,
+          non_analyzed_articles: 0,
+          articles_count: 0
+        };
+      }
+
+      aggregatedData[key].analysed_count += entry.analysed_count;
+      aggregatedData[key].non_analyzed_articles += entry.non_analyzed_articles;
+      aggregatedData[key].articles_count += entry.articles_count;
+    });
+
+    // Convert aggregated data to array and sort by date
+    const sortedData = Object.values(aggregatedData).sort((a, b) => a.timestamp - b.timestamp);
+    
+    // Fill in missing dates with zero values to avoid gaps
+    const filledData: any[] = [];
+    
+    if (sortedData.length > 0) {
+      const startTimestamp = sortedData[0].timestamp;
+      const endTimestamp = sortedData[sortedData.length - 1].timestamp;
+      
+      let currentTimestamp = startTimestamp;
+      
+      while (currentTimestamp <= endTimestamp) {
+        const currentDate = dayjs(currentTimestamp);
+        const currentKey = aggregationPeriod === 'day' 
+          ? currentDate.format('YYYY-MM-DD')
+          : currentDate.startOf('week').format('YYYY-MM-DD');
+        
+        // Find if we have data for this date
+        const existingData = sortedData.find(item => 
+          dayjs(item.timestamp).format('YYYY-MM-DD') === currentKey
+        );
+        
+        if (existingData) {
+          filledData.push(existingData);
+        } else {
+          // Add a zero-value entry for this date
+          filledData.push({
+            date: currentKey,
+            timestamp: currentTimestamp,
+            displayDate: currentDate.format('YYYY'),
+            analysed_count: 0,
+            non_analyzed_articles: 0,
+            articles_count: 0
+          });
+        }
+        
+        // Move to next date
+        currentTimestamp = aggregationPeriod === 'day'
+          ? currentTimestamp + 24 * 60 * 60 * 1000 // Add one day
+          : currentTimestamp + 7 * 24 * 60 * 60 * 1000; // Add one week
+      }
+    }
+    
+    return filledData;
   };
 
   const formatXAxis = (timestamp: number) => {
@@ -75,8 +170,8 @@ const ProceededArticlesGraph = ({ mediaId }: ProceededArticlesGraphProps) => {
   return (
     <BaseGraph graphName="proceed-article-graph" fetchUrl={fetchUrl} processData={processData}>
       {(data, loading) => {
-        // Filter the data based on domain range
-        const filteredData = filterDataByDomain(data);
+        // Filter and aggregate the data based on the number of data points
+        const aggregatedData = filterAndAggregateData(data);
         
         // Calculate domain for even distribution
         const [domainStart, domainEnd] = formattedDomainDateRange;
@@ -84,7 +179,7 @@ const ProceededArticlesGraph = ({ mediaId }: ProceededArticlesGraphProps) => {
         const endTimestamp = dayjs(domainEnd).valueOf();
 
         return (
-          <BarChart data={filteredData} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
+          <BarChart data={aggregatedData} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
             <XAxis 
               dataKey="timestamp"
               scale="time"
