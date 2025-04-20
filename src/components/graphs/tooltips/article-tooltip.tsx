28825fd656df4efb026d690f1e87109d79d0c02c
dufftie <dufftie@gmail.com>
2025-04-20 20:17:29 +0300

    Article tooltip improved

--- src/components/graphs/tooltips/article-tooltip.tsx
+++ src/components/graphs/tooltips/article-tooltip.tsx
@@ -5,28 +5,42 @@ import ArticlePreview from '@/src/components/article-preview';
 import { ExportOutlined } from '@ant-design/icons';
 import { formatDate } from '@/lib/utils/helpers';
 
+interface ArticleData {
+  article_id?: string;
+  authors?: string;
+  title?: string;
+  preview_url?: string;
+  date_time?: string;
+}
+
 const ArticleTooltip = (props: TooltipProps<any, any>) => {
-  const processData = (article: any) => {
-    return article;
+  const processData = (data: any[]): any[] => {
+    // If data is an array, return it as is
+    if (Array.isArray(data)) {
+      return data;
+    }
+    // If data is a single object, wrap it in an array
+    return [data];
   };
 
-  const article_id = props.payload[0]?.payload?.article_id;
+  const article_id = props.payload?.[0]?.payload?.article_id;
   const fetchUrl = article_id ? `/articles/${article_id}/tooltip` : undefined;
 
   return (
     <BaseTooltip fetchUrl={fetchUrl} processData={processData}>
-      {article => {
+      {(data: any[]) => {
+        const article = data[0] as ArticleData;
         return (
           <div className="article-tooltip">
             <ExportOutlined className="article-tooltip__icon" />
-            <div className="article-tooltip__authors">{article.authors}</div>
-            <div className="article-tooltip__title">{article.title}</div>
+            {article?.authors && <span className="article-tooltip__authors">{article.authors}</span>}
+            {article?.title && <span className="article-tooltip__title">{article.title}</span>}
             <ArticlePreview
-              preview_url={article.preview_url}
+              preview_url={article?.preview_url || ''}
               className="article-tooltip__preview"
             />
             <div className="article-tooltip__date">
-              {formatDate(article.date_time, 'DD MMMM YYYY')}
+              {article?.date_time && formatDate(article.date_time, 'DD MMMM YYYY')}
             </div>
           </div>
         );
