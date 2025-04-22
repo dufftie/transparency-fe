6c0754eca3d5378bcfda5a16a566223db5c701aa
dufftie <dufftie@gmail.com>
2025-04-23 02:17:13 +0300

    Implement scroll effect on header and update styles for improved responsiveness

--- src/components/layouts/main-layout/header.tsx
+++ src/components/layouts/main-layout/header.tsx
@@ -1,10 +1,28 @@
+'use client';
+
 import Link from 'next/link';
 import SupportButton from '@/components/support-button';
 import { Button } from 'antd';
+import { useEffect, useState } from 'react';
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  // TODO: Refactor this to be more smoother and more modern
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 20);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className={`header ${isScrolled ? 'header--scrolled' : ''}`}>
       <div className="header__inner">
         <Link href="/" className="header__logo">
           Ä
