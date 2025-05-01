b5e80c2e59baddf60584721835f6d8297deb7f8f
dufftie <dufftie@gmail.com>
2025-05-01 23:55:29 +0300

    Refactored card component

---
+++ src/components/card/card.tsx
@@ -0,0 +1,27 @@
+import classNames from 'classnames';
+import Hint from '../hint';
+import styles from './card.module.scss';
+
+interface CardProps {
+  label?: string;
+  children: JSX.Element;
+  primary?: boolean;
+  borderless?: boolean;
+  hint?: string;
+  className?: string;
+}
+
+export default function Card({ label, children, primary, borderless, hint, className }: CardProps) {
+  return (
+    <div className={classNames(styles.card, className, {
+      [styles.primary]: primary,
+      [styles.borderless]: borderless,
+    })}>
+      <div className={styles.header}>
+        {label && <div className={styles.label}>{label}</div>}
+        {hint && <Hint title={hint} />}
+      </div>
+      {children}
+    </div>
+  );
+}
