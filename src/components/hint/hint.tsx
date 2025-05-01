b3948f70716874114460eb52712d1f18d81d3b8b
dufftie <dufftie@gmail.com>
2025-05-02 00:13:51 +0300

    Refactored hint component

--- src/components/hint/hint.tsx
+++ src/components/hint/hint.tsx
@@ -2,15 +2,12 @@ import { QuestionCircleOutlined } from '@ant-design/icons';
 import { Popover } from 'antd';
 import React from 'react';
 import { PopoverProps } from 'antd/lib';
+import styles from './hint.module.scss';
 
-interface HintProps extends PopoverProps {}
-
-const Hint = (props: HintProps) => {
+export default function Hint(props: PopoverProps) {
   return (
-    <Popover className="hint" classNames={{ root: 'hint__inner' }} trigger="click" {...props}>
+    <Popover className={styles.hint} classNames={{ root: styles.hint }} trigger="click" {...props}>
       <QuestionCircleOutlined />
     </Popover>
   );
-};
-
-export default Hint;
+}
