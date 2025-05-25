8d116cbe93b05ddcccdeed0e9323e01a708c3ee4
dufftie <dufftie@gmail.com>
2025-05-25 12:29:16 +0200

    Updated sass import syntax; Removed redundant package-lock.json

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,10 @@ import createMDX from '@next/mdx'
 const nextConfig = {
     transpilePackages: ['@ant-design', 'antd'],
     pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+    sassOptions: {
+        includePaths: ['./src'],
+        prependData: `@use "sass:math";`,
+    },
 }
 
 const withMDX = createMDX({
